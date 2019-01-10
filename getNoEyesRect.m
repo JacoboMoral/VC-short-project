ecfce119aed97a7a3c9f7ff61dbb01dde4aab580
Jacobo Moral <jacobo.moral@gmail.com>
2019-01-10 16:38:41 +0100

    fix bug where it could grab an eye

--- getNoEyesRect.m
+++ getNoEyesRect.m
@@ -1,25 +1,25 @@
 function [rect] = getNoEyesRect(imageSize,forbiddenRect)
-    
+    forbiddenRect
     %a = xmin (horizontal)
-    a = randi(imageSize(1)-1)
+    a = randi(imageSize(2)-1)
     
     b = -1;
     %b = ymin (vertical)
     while b < 0
-        b = randi(imageSize(2)-1) 
+        b = randi(imageSize(1)-1) 
         if (b >= forbiddenRect(2) && b <= (forbiddenRect(2)+forbiddenRect(4)))
             b = -1;
         end
     end
     
-    c = randi(imageSize(1)-a)
+    c = randi(imageSize(2)-a)
     
     d = -1;
     %d = height (vertical)
     while d < 0
-        d = randi(imageSize(2)-b-1)
+        d = randi(imageSize(1)-b-1)
         
-        if ( (a <= forbiddenRect(1) && c >= (forbiddenRect(1) - a) || (a >= forbiddenRect(1) && (a <= (forbiddenRect(1)+c))) ) && (b <= forbiddenRect(2) && d >= (forbiddenRect(2) - b) || (b >= forbiddenRect(2) && (b <= (forbiddenRect(2)+d))) ))
+        if ( (a <= forbiddenRect(1) && c >= (forbiddenRect(1) - a) || (a >= forbiddenRect(1) && (a <= (forbiddenRect(1)+forbiddenRect(3)))) ) && (b <= forbiddenRect(2) && d >= (forbiddenRect(2) - b) || (b >= forbiddenRect(2) && (b <= (forbiddenRect(2)+forbiddenRect(4)))) ))
             d = -1;
         end
 
